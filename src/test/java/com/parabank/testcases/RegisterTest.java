ce695ed58adcccfece2ba18ec512253386ab0d48
Dilruba Sharmin <dilrubasharminqc@gmail.com>
2024-09-12 13:27:29 +0600

    Web automation Day 3 update for extend report with DDT Excel file for regiter page

--- src/test/java/com/parabank/testcases/RegisterTest.java
+++ src/test/java/com/parabank/testcases/RegisterTest.java
@@ -1,31 +1,82 @@
 package com.parabank.testcases;
 
-
 import com.parabank.pages.HomePage;
 import com.parabank.pages.LoginPage;
 import com.parabank.pages.RegisterPage;
+import com.parabank.util.General;
 import com.thedeanda.lorem.LoremIpsum;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class RegisterTest extends BaseTest{
-    @Test
-    public void registrationShouldFailWithData() {
-        LoginPage loginPage = page.getInstance(LoginPage.class);
-        RegisterPage registerPage = loginPage
-                .clickRegistrationLink()
-                .clickRegisterLink();
-        Assert.assertTrue(registerPage.hasFirstNameRequiredFieldError());
-        Assert.assertTrue(registerPage.hasError(10));
+
+public class RegisterTest extends BaseTest {
+
+        @Test
+        public void registrationShouldFailWithData() {
+            LoginPage loginPage = page.getInstance(LoginPage.class);
+            RegisterPage registerPage = loginPage.clickRegistrationLink()
+                    .clickRegisterLink();
+            Assert.assertTrue(registerPage.hasFirstNameRequiredFieldError());
+            Assert.assertTrue(registerPage.hasError(10));
+        }
+
+        @Test
+        public void registerShouldSucceed() {
+            String username = LoremIpsum.getInstance().getName();
+            LoginPage loginPage = page.getInstance(LoginPage.class);
+
+            RegisterPage registerPage = loginPage
+                    .clickRegistrationLink()
+                    .fillFirstName(LoremIpsum.getInstance().getFirstName())
+                    .fillLastName(LoremIpsum.getInstance().getLastName())
+                    .fillAddress(LoremIpsum.getInstance().getTitle(3))
+                    .fillCity(LoremIpsum.getInstance().getCity())
+                    .fillState(LoremIpsum.getInstance().getCity())
+                    .fillZipCode(LoremIpsum.getInstance().getZipCode())
+                    .fillPhone(LoremIpsum.getInstance().getPhone())
+                    .fillSsn("522-83-1308")
+                    .fillUsername(username)
+                    .fillPassword(username)
+                    .fillConfirmPassword(username);
+
+            HomePage homePage = registerPage
+                    .clickRegisterBtn();
+            Assert.assertTrue(homePage.hasLogoutLink());
+        }
+
+        @Test(enabled = false)
+        public void registerShouldSucceed2() {
+            String username = LoremIpsum.getInstance().getName();
+            HomePage homePage = page.getInstance(LoginPage.class)
+                    .clickRegistrationLink()
+                    .fillFirstName(LoremIpsum.getInstance().getFirstName())
+                    .fillLastName(LoremIpsum.getInstance().getLastName())
+                    .fillAddress(LoremIpsum.getInstance().getTitle(3))
+                    .fillCity(LoremIpsum.getInstance().getCity())
+                    .fillState(LoremIpsum.getInstance().getCity())
+                    .fillZipCode(LoremIpsum.getInstance().getZipCode())
+                    .fillPhone(LoremIpsum.getInstance().getPhone())
+                    .fillSsn("522-83-1308")
+                    .fillUsername(username)
+                    .fillPassword(username)
+                    .fillConfirmPassword(username)
+                    .clickRegisterBtn();
+            Assert.assertTrue(homePage.hasLogoutLink());
+        }
+    //test case using data from excel file
+    @DataProvider
+    public Object[][] getDataProviderData() {
+        return General.getTestData("Sheet1");
     }
 
-    @Test
-    public void registerShouldSucceed() {
+    @Test( dataProvider = "getDataProviderData")
+    public void registerShouldSucceed2(String firstName, String lastName) {
         String username = LoremIpsum.getInstance().getName();
         HomePage homePage = page.getInstance(LoginPage.class)
                 .clickRegistrationLink()
-                .fillFirstName(LoremIpsum.getInstance().getFirstName())
-                .fillLastName(LoremIpsum.getInstance().getLastName())
+                .fillFirstName(firstName)
+                .fillLastName(lastName)
                 .fillAddress(LoremIpsum.getInstance().getTitle(3))
                 .fillCity(LoremIpsum.getInstance().getCity())
                 .fillState(LoremIpsum.getInstance().getCity())
@@ -38,5 +89,5 @@ public class RegisterTest extends BaseTest{
                 .clickRegisterBtn();
         Assert.assertTrue(homePage.hasLogoutLink());
     }
+    }
 
-}
